979016556c9ed1ab6687b5c33f5ff893efbc7f76
Nicola <nicolalieggi099@gmail.com>
2021-12-03 19:11:51 +0100

    aggiunto metodo per la fattorizzazione LU con pivot

--- fattorizzazione_LU.py
+++ fattorizzazione_LU.py
@@ -1,4 +1,5 @@
-from numpy import *
+import numpy as np
+import scipy.linalg as linalg
 
 
 def triang_sup(a, b, tol=1e-15):
@@ -16,16 +17,16 @@ def triang_sup(a, b, tol=1e-15):
     Returns:
         vettore di float: vettore contenente i valori delle incognite x
     """
-    (r, c) = shape(a)
-    if r != size(b):
+    (r, c) = np.shape(a)
+    if r != np.size(b):
         raise Exception("Numero di equazini diverso dal numero di termini noti")
 
-    x = zeros((r, 1))
+    x = np.zeros((r, 1))
 
     for i in range(r-1, -1, -1):
         if abs(a[i][i]) < tol:
             print("determinante prossimo a zero")
-            return NaN
+            return np.NaN
 
         sum = 0
         for j in range(i+1, r):
@@ -52,16 +53,16 @@ def triang_inf(a, b, tol=1e-15):
     Returns:
         vettore di float: vettore contenente i valori delle incognite x
     """
-    (r, c) = shape(a)
-    if r != size(b):
+    (r, c) = np.shape(a)
+    if r != np.size(b):
         raise Exception("Numero di equazini diverso dal numero di termini noti")
     
-    x = zeros((r, 1))
+    x = np.zeros((r, 1))
     
     for i in range(0, r):
         if abs(a[i][i]) < tol:
             print("determinante prossimo a zero")
-            return NaN
+            return np.NaN
 
         sum = 0
         for j in range(0, i):
@@ -87,13 +88,12 @@ def fattlu(m):
         matrice U: matrice triangolare superiore U
     """
     
-    a = copy(m)
-    
-    n = a.shape[0]
-    if n != a.shape[1]:
+    n = m.shape[0]
+    if n != m.shape[1]:
         raise Exception("Matrice non quadrata")
     
-    l = identity(n)
+    a = np.copy(m)
+    l = np.identity(n)
     for k in range(n-1):
         for i in range(k+1, n):
             m = -(a[i][k]/a[k][k])
@@ -101,16 +101,31 @@ def fattlu(m):
                 a[i][j] = a[i][j]+(m*a[k][j])
             l[i][k] = -m
     
-    return l, triu(a)
+    return l, np.triu(a)
 
 
-def gauss_max_pivot_par(a_, b_, tol=1e-15):
+def gauss_max_pivot_par(a_, b_):
+    """trasforma il sistema lineare dalla forma Ax = b alla forma Ux = b
+    (vedi fattorizzazione LU) con l'algoritmo di elimizazione di gauss sfruttando il max pivot per rendere l'algoritmo più stabile
+
+    Args:
+        a_ (matrice di float): matrice dei coefficienti
+        b_ (vettore di float): vettore dei termini noti
+
+    Raises:
+        Exception: eccezione generica in caso di matrice non quadrata
+
+    Returns:
+        matrice di float: matrice triangolare superiore
+        vettore di float: nuovo vettore dei termini noti 
+    """
+    
     n = a_.shape[0]
     if n != a_.shape[1]:
         raise Exception("Matrice non quadrata")
     
-    a = copy(a_)
-    b = copy(b_)
+    a = np.copy(a_)
+    b = np.copy(b_)
         
     for k in range(n-1):
         s = k
@@ -131,21 +146,52 @@ def gauss_max_pivot_par(a_, b_, tol=1e-15):
             b[i] = b[i]+(m*b[k])
                     
     
-    return triu(a), b
+    return np.triu(a), b
 
+
+def fattlu_pivot(m):
+    """fattorizza m sfruttando la fattorizzazione LU con tecnica di pivot
+
+    Args:
+        m (matrice di float): matrice da fattorizzare
+
+    Raises:
+        Exception: eccezione generica in caso matrice non quadrata
+
+    Returns:
+        matrice di 0 e 1: matrice di permutazione
+        matrice di float: matrice triangolare inferiore speciale L
+        matrice di float: matrice triangolare superiore U
+    """
+    
+    n = m.shape[0]
+    if n != m.shape[1]:
+        raise Exception("Matrice non quadrata")
+    
+    p = np.arange(0, n)
+    a = np.copy(m)
+    for k in range(n-1):
+        max_row_index = np.argmax(abs(a[k:n, k])) + k
+        p[[k, max_row_index]] = p[[max_row_index, k]]
+        a[[k, max_row_index]] = a[[max_row_index, k]]
+        
+        for i in range(k+1, n):
+            a[i][k] /= a[k][k]
+            for j in range(k+1, n):
+                a[i][j] -= a[i][k]*a[k][j]            
+    
+    l = np.tril(a, -1)
+    np.fill_diagonal(l, 1)
+    return p, l, np.triu(a)
 
 def main():
-    a = array([[1, -2, 3], [-1, 2, 1], [2, -1, -1]], float)
-    b = array([1, 0, -1], float)
+    a = np.array([[2,1,0,-1],[-2,-2,1,-1],[4,2,-1,-1],[0,2,-3,2]], float)
+    #a = np.array([[1, 2, 4], [2, 1, 3], [3, 2, 4]], float)
     
-    #a = array([[1, -2, 0, 1], [-1, 1, 2, 0], [2, -3, 0, 2], [-1, 2, 0, 2]], float)
-    #b = array([-1, 0, 1, 2], float)
-    
-    [x, y] = gauss_max_pivot_par(a, b)
-    print(x, "\n")
-    print(y, "\n")
-    
-    print(triang_sup(x, y))    
+    [p, l, u] = fattlu_pivot(a)
+    print(p, "\n")
+    print(l, "\n")
+    print(u, "\n")
     
     return
 
